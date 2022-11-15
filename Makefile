ff90562882aafd4c9e2624c7cee5d58a717b25ab
ZhdanovP <pavel.zhdanov@neuralegion.com>
2022-11-15 14:51:47 +0200

    add git hooks

---
+++ Makefile
@@ -0,0 +1,12 @@
+.PHONY: deploy_hooks
+deploy_hooks:
+	chmod +x hooks/deploy_hooks.sh
+	./hooks/deploy_hooks.sh
+
+.PHONY: run
+run:
+	python main.py
+
+.PHONY: all
+all: deploy_hooks run
+	
---
+++ hooks/deploy_hooks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd $(dirname ${BASH_SOURCE[0]})
+
+# cp -rf ./pre-commit ../.git/hooks/pre-commit
+# cp -rf ./post-merge ../.git/hooks/post-merge
+cp -rf ./commit-msg ../.git/hooks/commit-msg
+
+if [ $? -eq 0 ]; then
+ # copy ok
+ exit 0
+else
+ # copy failed
+ exit 1
+fi
